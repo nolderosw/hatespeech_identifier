70cf0a8cea31a1b57b371b37ecc783c2e2d1a0fb
Wesley Azevedo <wesley150@MacBook.local>
2022-04-26 22:18:52 -0300

    collector

--- collector/collector.py
+++ collector/collector.py
@@ -4,6 +4,6 @@ import tweepy
 client = authenticate()
 
 query = 'odio -is:retweet -is:reply'
-tweets = client.search_recent_tweets(query=query, max_results=10)
+tweets = client.search_recent_tweets(query=query, max_results=1)
 for tweet in tweets.data:
     print(tweet.text, '\n')
